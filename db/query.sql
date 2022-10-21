e6819c0a594b59137febc5d0dac3575b3fdc364c
Chibuike Okoye~ <talk2chibyke@yahoo.com>
2022-10-21 23:46:17 +1100

    finished writing query

--- db/query.sql
+++ db/query.sql
@@ -1,17 +1,13 @@
 SELECT 
 
-employee.id As id
+employee.id As id,
+employee.first_name AS first_name,
+employee.last_name AS last_name,
 roles.title AS title,
-roles.salary AS salary,
 department.name AS department,
-employee.first_name AS first_name
-employee.last_name AS last_name
+roles.salary AS salary,
 employee.manager_id As manager_id
 
-
-FROM roles
-JOIN department ON roles.department = department.id;
-
-SELECT *
 FROM employee
-JOIN roles ON employee.roles = employee.id
+JOIN department ON employee.department = department.id
+JOIN roles ON employee.roles = employee.id;
