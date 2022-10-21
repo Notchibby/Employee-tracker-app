94048864c05dd8d189b78ebf11241675ddc427fd
Chibuike Okoye~ <talk2chibyke@yahoo.com>
2022-10-22 01:48:26 +1100

    finished writing one of the queries

--- db/query.sql
+++ db/query.sql
@@ -6,9 +6,11 @@ employee.last_name AS last_name,
 role.title AS title,
 department.name AS department,
 role.salary AS salary,
-employee.manager_id As manager_id
+employee.manager_id As manager_id,
+CONCAT(manager.first_name, ' ', manager.last_name) As manager
 
 FROM employee
-JOIN department ON employee.department = department.id
-JOIN role ON employee.role = role.id
-JOIN manager_id ON employee.id = employee.manager_id;
+LEFT JOIN department ON employee.department = department.id
+LEFT JOIN role ON employee.role = role.id
+LEFT JOIN employee manager ON manager.id = employee.manager_id
+
